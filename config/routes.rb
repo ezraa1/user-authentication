4b06a825086fc58c4d53c5110d6b80ef8d7448f3
ezraa <ezrakipchirchir1034@gmail.com>
2023-06-14 07:29:00 -0400

    create user sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+root 'sessions#home'
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # users routes
+  resources :users, only: [ :new, :create, :edit, :update, :show, :destroy]
+
+  # sessions routes
+  get '/login', to: 'sessions#login'
+  post '/login', to: 'sessions#create'
+  post '/logout', to: 'sessions#destroy'
+  get '/logout', to: 'sessions#destroy'
 end
